0c931dc89be8483426c4d3adea6964e25764981c
Jose <oladotjos@gmail.com>
2025-12-08 17:56:08 +0100

    feat:update main.tsx to  serve as entry point for a library

--- packages/widget/src/main.tsx
+++ packages/widget/src/main.tsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import TourComponent from './TourComponent';
+import { initializeWidget } from './loading';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+(window as any).renderTourUI = function(tourConfig: any) {
+  
+    const rootElement = document.createElement('div');
+    rootElement.id = 'tourguidekit-root';
+    document.body.appendChild(rootElement);
+
+    const shadowRoot = rootElement.attachShadow({ mode: 'open' });
+    const container = document.createElement('div');
+    shadowRoot.appendChild(container);
+
+    ReactDOM.createRoot(container).render(
+        <React.StrictMode>
+            <TourComponent steps={tourConfig.steps} globalTourId={tourConfig.id} />
+        </React.StrictMode>
+    );
+};
+
+
+initializeWidget();
